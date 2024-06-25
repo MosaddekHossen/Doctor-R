3eef2d3c77f8ea563a16c919d08d36d68d5ea320
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-06-26 02:47:46 +0600

    added responsive navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer";
+import Navbar from "../Pages/Shared/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'custom': '1140px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
